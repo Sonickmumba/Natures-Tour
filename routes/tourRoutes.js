90dad2d3ebc1074562e900d36d118f1ffba660a3
Sonick Mumba <kingellie.mumba@gmail.com>
2024-10-29 08:24:30 +0200

    Update tourRoutes.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,9 +4,10 @@ const tourController = require('../controllers/tourController')
 const tourRouter = express.Router();
 
 tourRouter.param('id', tourController.checkId);
+// tourRouter.param('')
 
 tourRouter.get("/", tourController.getAllTours);
-tourRouter.post("/", tourController.createTour);
+tourRouter.post("/", tourController.checkBody, tourController.createTour);
 tourRouter.get("/:id", tourController.getTourById);
 tourRouter.patch("/:id", tourController.updateTour);
 tourRouter.delete("/:id", tourController.deleteTour);
