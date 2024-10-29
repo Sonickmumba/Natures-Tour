1c9b29bed0dc61f2d0e3b170260a61d70188595b
Sonick Mumba <kingellie.mumba@gmail.com>
2024-10-29 03:00:59 +0200

    Create tourRoutes.js

---
+++ routes/tourRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const tourController = require('../controllers/tourController')
+
+const tourRouter = express.Router();
+
+tourRouter.get("/", tourController.getAllTours);
+tourRouter.post("/", tourController.createTour);
+tourRouter.get("/:id", tourController.getTourById);
+tourRouter.patch("/:id", tourController.updateTour);
+tourRouter.delete("/:id", tourController.deleteTour);
+
+
+
+
+
+module.exports = tourRouter;
